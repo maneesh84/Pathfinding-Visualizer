49a3545b0ecce517675341b15e04a168ff5e08a0
maneesh84 <maneeshkumar849896@gmail.com>
2025-09-22 17:11:04 +0530

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,18 @@ import { Grid } from "./components/Grid";
 import { PathfindingProvider } from "./contexts/PathfindingContext";
 import { SpeedProvider } from "./contexts/SpeedContext";
 import { TileProvider } from "./contexts/TileContext";
+import { Nav } from "./components/Nav";
 
 function App() {
-
-  const isVisualizationRunningRef=useRef(false);
+  const isVisualizationRunningRef = useRef(false);
   return (
     <>
       <PathfindingProvider>
         <TileProvider>
           <SpeedProvider>
-            <div className="h-screen w-screen flex flex-col bg-gray-950">
-              <Grid isVisualizationRunningRef={isVisualizationRunningRef}/>
+            <div className="h-screen w-screen flex flex-col ">
+              <Nav />
+              <Grid isVisualizationRunningRef={isVisualizationRunningRef} />
             </div>
           </SpeedProvider>
         </TileProvider>
